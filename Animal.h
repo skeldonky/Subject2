09c62495ba0f5eeecc55b3f1f1fba5235afc156b
skeldonky <75779367+skeldonky@users.noreply.github.com>
2024-12-24 20:19:17 +0900

    초기 커밋.

---
+++ Animal.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+//부모 클래스 Animal
+class Animal {
+
+public:
+	Animal(){}
+	//가상함수 자식 클래스에서 새로 정의 가능
+	virtual void makeSound(){}
+	virtual ~Animal(){}
+};
+
+//자식클래스1 Dog
+class Dog :public Animal {
+
+public:
+	Dog(string sound):c_sound(sound){}
+	void makeSound() { cout << "Dog Sound : " << c_sound << endl; }
+private:
+	string c_sound;
+};
+
+//자식 클래스2 Cat
+class Cat :public Animal {
+public:
+	Cat(string sound) :c_sound(sound) {}
+	void makeSound() { cout << "Cat Sound : " << c_sound << endl; }
+private:
+	string c_sound;
+};
+
+//자식 클래스3 Cow
+class Cow :public Animal {
+public:
+	Cow(string sound) :c_sound(sound) {}
+	void makeSound() { cout << "Cow Sound : " << c_sound << endl; }
+private:
+	string c_sound;
+};
+
+//다형성 crying에 dog, cat , cow를 넣어 makeSound를 호출
+void crying(Animal* animal)
+{
+	animal->makeSound();
+}
---
+++ main.cpp
@@ -0,0 +1,24 @@
+//과제 2
+#include <iostream>
+//헤더 참조
+#include "Animal.h"
+
+using namespace std;
+
+int main()
+{
+    //동물 울음소리
+    Dog Dog("Bow Bow");
+    Cat Cat("Meow~");
+    Cow Cow("Moooooo");
+
+    //Animal을 포인터 타입으로 생성
+    Animal* animal[] = {&Dog, &Cat, &Cow};
+
+    for (int i = 0; i < 3; i++)
+    {
+        crying(animal[i]);
+    }
+
+    return 0;
+}
